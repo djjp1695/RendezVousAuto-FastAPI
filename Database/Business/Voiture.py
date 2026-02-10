f40104f7ab1d1312a192747ab9853d9c272faeee
Jean-Philippe Chalifoux <jp1695@gmail.com>
2026-02-10 12:02:44 -0500

    Début projet

--- Database/Business/Voiture.py
+++ Database/Business/Voiture.py
@@ -1,9 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
 from sqlmodel import SQLModel, Field
-from typing import Annotated
-
-Base = declarative_base()
-
 
 class Voiture(SQLModel):
     id: int | None = Field(primary_key=True, autoincrement=True, )

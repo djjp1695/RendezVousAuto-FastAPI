0db81ab05c396430f850c172266cf2a6515d9135
Jean-Philippe Chalifoux <jp1695@gmail.com>
2026-02-14 16:57:27 -0500

    Revert "Changement pour module et export import"
    This reverts commit 8958618427dc4f9f28dfcf7331137474f979c548.

---
+++ www/static/Services/VoitureService.js
@@ -0,0 +1,15 @@
+class VoitureService {
+    async getAll() {
+        try {
+            const response = await fetch('/Voitures')
+            if (!response.ok)
+                console.error(response.status);
+            const data = await response.json();
+            return data;
+        }
+        catch (err) {
+            console.error(err);
+        }
+
+    }
+}

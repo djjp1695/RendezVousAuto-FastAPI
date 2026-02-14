8958618427dc4f9f28dfcf7331137474f979c548
Jean-Philippe Chalifoux <jp1695@gmail.com>
2026-02-13 20:03:31 -0500

    Changement pour module et export import

--- www/static/Managers/VoitureManager.js
+++
@@ -1,9 +0,0 @@
-class VoitureManager {
-    constructor() {
-        this.voitureService = new VoitureService();
-    }
-
-    async getAllVoitures() {
-        return await this.voitureService.getAll();
-    }
-}
